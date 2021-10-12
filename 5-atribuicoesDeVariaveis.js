50cee34d92f1e18553a902ceae610e29885b13dd
Aliny <aliny.souz.r@gmail.com>
2021-10-12 19:42:37 -0400

    arquivo 5 JS modificado

--- 5-atribuicoesDeVariaveis.js
+++ 5-atribuicoesDeVariaveis.js
@@ -8,3 +8,4 @@ console.log(primeiroNome, sobrenome);
 console.log(`Meu nome é ${primeiroNome} ${sobrenome}`); //em "template literals" você deve passar crase no lugar de aspas simples ou duplas
 
 const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
