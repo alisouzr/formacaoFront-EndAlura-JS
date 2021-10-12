b6cb81214870a655fd71b88191fabc12ed03c3a6
Aliny <aliny.souz.r@gmail.com>
2021-10-12 19:35:55 -0400

    Update quinto arquivo JS

---
+++ 5-atribuicoesDeVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+console.log(primeiroNome, sobrenome);
+
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`); //em "template literals" você deve passar crase no lugar de aspas simples ou duplas
+
+const nomeCompleto = primeiroNome + sobrenome;
