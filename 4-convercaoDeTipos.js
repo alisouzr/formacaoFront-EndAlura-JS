9da3fb65b019b3e0d07723b298fa6be656c8cd93
Aliny <aliny.souz.r@gmail.com>
2021-10-12 15:27:16 -0400

    quarto arquivo criado

---
+++ 4-convercaoDeTipos.js
@@ -0,0 +1,8 @@
+console.log("Conversão de Tipos");
+
+console.log("ano " + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+//o JS interpreta automaticamente que é número,pois divisão é somente com números
+console.log("10" / "2");
