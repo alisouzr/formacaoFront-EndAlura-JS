782d80ffec422eba8a6e61cc68311f0aad268fba
Aliny <aliny.souz.r@gmail.com>
2021-10-12 19:58:56 -0400

    Update arquivo Javascript

---
+++ 6-listas.js
@@ -0,0 +1,19 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+console.log(`Destinos possíveis :`);
+// console.log(salvador, saoPaulo, rioDeJaneiro);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`);
+listaDeDestinos.splice(1, 1);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[1], listaDeDestinos[0]);
