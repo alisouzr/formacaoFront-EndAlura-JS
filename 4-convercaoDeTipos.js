83105f8de7d3662f30b18f3ec07eda33be7b798d
Aliny <aliny.souz.r@gmail.com>
2021-10-12 15:30:33 -0400

    Update quato arquivo JS

--- 4-convercaoDeTipos.js
+++ 4-convercaoDeTipos.js
@@ -4,5 +4,5 @@ console.log("ano " + 2020);
 console.log("2" + "2");
 console.log(parseInt("2") + parseInt("2"));
 
-//o JS interpreta automaticamente que é número,pois divisão é somente com números
-console.log("10" / "2");
+console.log("10" / "2"); //o JS interpreta automaticamente que é número,pois divisão é somente com números
+console.log("Ricardo" / "2"); //Not a number
