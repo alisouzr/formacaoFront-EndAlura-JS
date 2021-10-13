0ed9c4dd45c68638b3102a2167ef7e35ec9acf54
Aliny <aliny.souz.r@gmail.com>
2021-10-12 22:46:22 -0400

    Update arquivo de condicionais JS

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,15 +6,26 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
+const estaAcompanhada = true;
 const idadeComprador = 15;
 console.log(`Destinos possíveis :`);
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1);//removendo item
-
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa Viagem");
+    listaDeDestinos.splice(1, 1); //removedo item
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
+
+if (idadeComprador >= 18) {
+    console.log("Comprador maior de idadade");
+    listaDeDestinos.splice(1, 1); // removendo item
+} else if (estaAcompanhada == true) {
+    console.log("Comprador está acompanhado");
+    listaDeDestinos.splice(1, 1); //removedo item
+} else {
+    console.log("Não é maior de idade e não posso vender");
+}
+
 console.log(listaDeDestinos);
